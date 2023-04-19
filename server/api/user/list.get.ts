34c07ee5b5354586000de7b5341870cd32df5ca1
YYB <512060925@qq.com>
2023-04-19 09:47:45 +0800

    测试

--- server/api/user/list.get.ts
+++ server/api/user/list.get.ts
@@ -1,8 +1,10 @@
 import UserModel from '~/server/models/user'
+const { mysql } = useRuntimeConfig()
 export default defineEventHandler(async event => {
   const res = await UserModel.findAll()
   console.log(res)
   return {
     code: 0,
+    mysql,
   }
 })
