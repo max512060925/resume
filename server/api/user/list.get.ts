a7becb07170d0ac5b7966c2ac3513d8cc43572a1
DESKTOP-Q5JA64F\YYB <512060925@qq.com>
2023-04-26 00:13:16 +0800

    修复路由权限

--- server/api/user/list.get.ts
+++
@@ -1,9 +0,0 @@
-const { mysql } = useRuntimeConfig()
-export default defineEventHandler(async event => {
-  const res = await UserModel.findAll()
-  console.log(res)
-  return {
-    code: 0,
-    mysql,
-  }
-})
