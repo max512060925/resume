967040f9532210f6b5251aa2c8fef1da26a43e8c
DESKTOP-Q5JA64F\YYB <512060925@qq.com>
2023-04-26 23:55:22 +0800

    增加磁盘列表

--- server/api/disk/list.get.ts
+++ server/api/disk/list.get.ts
@@ -1,17 +1,37 @@
-import { readdir } from 'fs/promises'
+import { readdir, stat } from 'fs/promises'
+import { resolve, join } from 'path'
+import { fileTypeFromFile } from 'file-type'
+
 const {
   disk: { path },
 } = useRuntimeConfig()
-
+const basePath = resolve(path)
 export default defineEventHandler(async event => {
   try {
-    // if (event.context.uid) {
-    const files = await readdir(path)
-    console.log(files)
-    return resJsonBody(files)
-    // } else {
-    //   return failJsonBody(403, '账号未登录！')
-    // }
+    if (event.context.uid) {
+      const files = await readdir(basePath, { withFileTypes: true })
+      return resJsonBody(
+        await Promise.all(
+          files.map(async file => {
+            const filePath = join(basePath, file.name)
+            const { size, mtime, birthtime } = await stat(filePath)
+            const isFile = file.isFile()
+            return {
+              name: file.name,
+              isDirectory: file.isDirectory(),
+              isFile,
+              createTime: birthtime,
+              updateTime: mtime,
+              size,
+              ...(isFile &&
+                (await fileTypeFromFile(join(basePath, file.name)))),
+            }
+          })
+        )
+      )
+    } else {
+      return failJsonBody(403, '账号未登录！')
+    }
   } catch (e) {
     return failJsonBody(500, String(e), event)
   }
